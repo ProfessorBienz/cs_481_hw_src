e40fec83aa7261d0d2632a4558ee5379772b7f58
Amanda Bienz <bienz@Amandas-Work-MacBook-Pro.local>
2023-01-19 13:33:56 -0700

    Updating code

--- src.hpp
+++ src.hpp
@@ -3,8 +3,35 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
-int return1();
-int return0();
+void create_processes();
+
+void grandchild();
+void child();
+void parent();
+
+
+
+void priority(int n_jobs, Job* jobs);
+void priority_rr(int n_jobs, Job* jobs, int time_slice);
+
+typedef void (*Operation)();
+typedef struct job
+{
+    int idx;
+    int time;
+    int priority;
+    Operation run_job;
+} Job;
+
+int jobcmp(const void* a, const void* b);
+void job0(int time);
+void job1(int time);
+void job2(int time);
+void create_jobs(int* n_jobs, Job** jobs);
 
 #endif
