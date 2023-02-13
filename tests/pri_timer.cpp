64bd4c0695bd2f4a94f6f1d6cccdaf52557625ed
Amanda Bienz <bienz@amandas-mbp.unm.edu>
2023-02-13 15:16:00 -0700

    Added timing options

---
+++ tests/pri_timer.cpp
@@ -0,0 +1,80 @@
+#include "cs_481_hw_src/src.hpp"
+#include "timer.h"
+
+int counter;
+void job0(int time)
+{
+    for (int t = 0; t < time; t++)
+        counter += 1;
+}
+
+void job1(int time)
+{
+    for (int t = 0; t < time; t++)
+        counter += 1;
+}
+
+void job2(int time)
+{
+    for (int t = 0; t < time; t++)
+        counter += 1;
+}
+
+void create_jobs(int* n_jobs, Job** jobs)
+{
+    *n_jobs = 5;
+    Job* example_jobs = (Job*)malloc((*n_jobs)*sizeof(Job));
+
+    int priorities[] = {1, 1, 0, 2, 4};
+    int indices[] = {2, 0, 1, 3, 4};
+    int times[] = {4, 5, 3, 3, 2};
+    Operation run_jobs[] = {job0, job1, job2, job0, job1};
+
+    for (int i = 0; i < *n_jobs; i++)
+    {
+        example_jobs[i].priority = priorities[i];
+        example_jobs[i].idx = indices[i];
+        example_jobs[i].time = times[i];
+        example_jobs[i].run_job = run_jobs[i];
+    }
+
+    *jobs = example_jobs;
+}
+
+
+
+
+int main(int argc, char* argv[])
+{
+    int n_jobs;
+    Job* jobs;
+    int n_iter = 25000000;
+
+    // Run Test
+    create_jobs(&n_jobs, &jobs);
+    counter = 0;
+
+    double t0, tfinal;
+    t0 = get_time();
+
+    for (int i = 0; i < n_iter; i++)
+    {  
+        priority(n_jobs, jobs);
+    }
+    tfinal = (get_time() - t0) / n_iter;
+
+    printf("Priority Scheduler Takes %e Seconds\n", tfinal);    
+
+
+    t0 = get_time();
+
+    for (int i = 0; i < n_iter; i++)
+    {  
+        priority_rr(n_jobs, jobs, 3);
+    }
+    tfinal = (get_time() - t0) / n_iter;
+
+    printf("Priority RR Scheduler Takes %e Seconds\n", tfinal);    
+    
+    free(jobs);
+}
