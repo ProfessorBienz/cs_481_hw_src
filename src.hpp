b45361a53d9537fac6a1c86e70f043703f4506ab
Amanda Bienz <bienz@Amandas-MacBook-Pro-2.local>
2023-04-14 15:51:33 -0600

    Added initial tests

--- src.hpp
+++ src.hpp
@@ -1,57 +1,16 @@
 #ifndef TEST_SRC_HPP
 #define TEST_SRC_HPP
 
-#include <pthread.h>
-#include <semaphore.h>
-#include "stdlib.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 
 
-// Dining Philosophers
-typedef struct __diners_t
-{
-    sem_t** forks;
-    int size;
-    pthread_mutex_t mutex;
-    int philosopher;
-    bool* eat;
-} diners_t;
-
-void eat(diners_t* diner);
-sem_t* left(diners_t* diner);
-sem_t* right(diners_t* diner);
-
-// For you to implement
-void* philosopher(void* args);
-
-
-typedef struct __buffer_t
-{
-    int limit;
-    int* list;
-    int put_ctr;
-    int get_ctr;
-    int size;
-    pthread_cond_t empty;
-    pthread_cond_t full;
-    pthread_mutex_t mutex;
-    sem_t* sem_empty;
-    sem_t* sem_full;
-} buffer_t;
-
-void init(buffer_t* buf, int limit);
-void put(buffer_t* buf, int val);
-void get(buffer_t* buf, int* val);
-void destroy(buffer_t* buf);
-
-
-typedef struct __data_t
-{
-    buffer_t* buf;
-    int val;
-} data_t;
-
-void* producer_thread(void* args);
-void* consumer_thread(void* args);
-
+void write_to_journal(int txb_bytes, char* txb, int ibytes, char* i,
+        int bbytes, char* b, int Db_bytes, char* Db, int txe_bytes, char* txe);
+int checkpoint(int txe_bytes, char* txe);
 
 #endif
